8c89f867199a17eed61fc33006a382ff05398a9d
Raguso <mir9195@sis.nyp.org>
2020-12-01 17:29:44 -0500

    part 1 of DOM exercises

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+const p = document.querySelector('p'); 
+const divs = document.querySelectorAll('div'); 
+
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+
+console.log(p);
+console.log(divs);
