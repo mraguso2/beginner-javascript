c5df17c022ace1d414fc0d4caf67cefba145d670
Michael Raguso <michael.raguso@gmail.com>
2021-05-05 21:37:43 -0400

    tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,22 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach(panel => panel.hidden = true);
+
+  // mark all tabs as unselected
+  tabButtons.forEach(tab => tab.setAttribute('aria-selected', false));
+
+  // mark clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  // find associated panel and show it
+  const { id } = event.currentTarget;
+  const matchingTabPanel = document.querySelector(`[aria-labelledby="${id}"`);
+  matchingTabPanel.hidden = false;
+}
+
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
