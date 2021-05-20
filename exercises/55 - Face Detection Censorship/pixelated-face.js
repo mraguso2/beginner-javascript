afaef15f3fe50e8efff11e2b9b5f7dd7152474ac
Raguso <mir9195@sis.nyp.org>
2021-05-20 07:49:15 -0400

    face detection

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,8 +1,11 @@
 const video = document.querySelector('.webcam');
+
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
+
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
+
 const faceDetector = new window.FaceDetector();
 const optionsInputs = document.querySelectorAll(
   '.controls input[type="range"]'
@@ -21,11 +24,12 @@ optionsInputs.forEach(input => input.addEventListener('input', handleOption));
 
 // Write a fucntion that will populate the users video
 async function populateVideo() {
+  // getUserMedia is a promise
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 1280, height: 720 },
+    video: { width: 1280, height: 720 }, 
   });
   video.srcObject = stream;
-  await video.play();
+  await video.play(); // wait for video to play & THEN set width & height
   // size the canvases to be the same size as the video
   console.log(video.videoWidth, video.videoHeight);
   canvas.width = video.videoWidth;
@@ -39,12 +43,12 @@ async function detect() {
   // ask the browser when the next animation frame is, and tell it to run detect for us
   faces.forEach(drawFace);
   faces.forEach(censor);
-  requestAnimationFrame(detect);
+  requestAnimationFrame(detect); // reeecuurrrrssiioonn
 }
 
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // everytime it runs, it clears out the canvas
   ctx.strokeStyle = '#ffc600';
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
